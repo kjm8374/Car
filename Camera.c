ac8193bb0e1b3c1e5a74812b160dcd38e27eacea
Thomas <taf2981@rit.edu>
2022-03-25 16:49:55 -0400

    fixed some errors. compiles now

--- Camera.c
+++ Camera.c
@@ -89,54 +89,54 @@ void INIT_Camera(void)
 //
 //
 /////////////////////////////////////////////////////
-int main(void)
-{
-	int i = 0;
-	//initializations
-	DisableInterrupts();
-	uart0_init();
-	uart0_put("\r\nLab5 CAMERA demo\r\n");
+//int main(void)
+//{
+//	int i = 0;
+//	//initializations
+//	DisableInterrupts();
+//	uart0_init();
+//	uart0_put("\r\nLab5 CAMERA demo\r\n");
 
-	
-	uart0_put("\r\nINIT LEDs\r\n");
-	LED1_Init();
-	LED2_Init();
-	// remember that we double the desired frequency because we need to account
-	// for the toggle from hi to low.
-	//
-	//Output_MCLK();
-	uart0_put("\r\nINIT Camera CLK and SI\r\n");
-	uart0_put("\r\nINIT ADC\r\n");	
-	INIT_Camera();
-	
-	uart0_put("\r\nINIT Switch 2\r\n");
-	Switch2_Init();
+//	
+//	uart0_put("\r\nINIT LEDs\r\n");
+//	LED1_Init();
+//	LED2_Init();
+//	// remember that we double the desired frequency because we need to account
+//	// for the toggle from hi to low.
+//	//
+//	//Output_MCLK();
+//	uart0_put("\r\nINIT Camera CLK and SI\r\n");
+//	uart0_put("\r\nINIT ADC\r\n");	
+//	INIT_Camera();
+//	
+//	uart0_put("\r\nINIT Switch 2\r\n");
+//	Switch2_Init();
 
 
-	uart0_put("\r\nEnable Interrupts\r\n");
-	EnableInterrupts();
-	uart0_put("\r\nInterrupts successfully enabled\r\n");
+//	uart0_put("\r\nEnable Interrupts\r\n");
+//	EnableInterrupts();
+//	uart0_put("\r\nInterrupts successfully enabled\r\n");
 
-	while(1)
-	{
+//	while(1)
+//	{
 
-		if (g_sendData == TRUE) 
-		{
-			LED1_On();
-			// send the array over uart
-			sprintf(str,"%i\n\r",-1); // start value
-			uart0_put(str);
-			for (i = 0; i < 128; i++) 
-			{
-				sprintf(str,"%i\n\r", line[i]);
-				uart0_put(str);
-			}
-			sprintf(str,"%i\n\r",-2); // end value
-			uart0_put(str);
-			LED1_Off();
-			g_sendData = FALSE;
-		}
-		// do a small delay
-		myDelay();
-	}
-}
+//		if (g_sendData == TRUE) 
+//		{
+//			LED1_On();
+//			// send the array over uart
+//			sprintf(str,"%i\n\r",-1); // start value
+//			uart0_put(str);
+//			for (i = 0; i < 128; i++) 
+//			{
+//				sprintf(str,"%i\n\r", line[i]);
+//				uart0_put(str);
+//			}
+//			sprintf(str,"%i\n\r",-2); // end value
+//			uart0_put(str);
+//			LED1_Off();
+//			g_sendData = FALSE;
+//		}
+//		// do a small delay
+//		myDelay();
+//	}
+//}
