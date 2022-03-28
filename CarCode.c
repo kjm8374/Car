d302753157d49f1bf5ae22c8e822ba43cf887984
Kushal <kjm8374@rit.edu>
2022-03-28 12:09:29 -0400

    Can't find right peak, left peak detected

--- CarCode.c
+++ CarCode.c
@@ -28,28 +28,25 @@
 #include "uart.h"
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
-int currentLocation = 0;
-int leftPeakLoc = 0;
-int rightPeakLoc = 0;
-int dif =0;
-float CalculatedPosition =65;
-int trackWidth =128;
+
 double normalized_trace[128];
 double filter[131];
 
 
 extern uint16_t line[128];
 extern BOOLEAN g_sendData;
-uint32_t SmoothLine[128];
+uint16_t SmoothLine[128];
 //found dif line needs to be a u int 32
 uint32_t DiffLine[128];
 BOOLEAN debug;
 
-void CalculatePeakLocations(void);
-void evaluatePositionANDTurn(int LeftLocation, int RightLocation);
+void CalculatePeakLocations(int* leftPeakLoc, int* rightPeakLoc);
+void evaluatePositionANDTurn(int* leftPeakLoc, int* rightPeakLoc, int* CalculatedPosition);
 void FilterLine(void);
 void myDelay(int del);
+int max(void);
 void TurnRightPercent(double percentage);
 void TurnLeftPercent(double percentage);
 void Normalize(void);
@@ -70,10 +67,10 @@ void myDelay(int del){
 void initialize() {
     
     uint16_t motor_period = 48000000/10000;
-    uint16_t servo_period = 3000000/50;
+    uint32_t servo_period = 3000000/50;
     DisableInterrupts();
     uart0_init();
-    uart0_put("Uart Initialized\r\n");
+    //uart0_put("Uart Initialized\r\n");
     //init leds for debugging etc
     LED1_Init();
     LED2_Init();
@@ -88,16 +85,20 @@ void initialize() {
     
 }
 int main(){
-    char str[200];
+	  char str[200];
     int i=0;
-    debug = FALSE;
+	  int leftPeakLoc = 0;
+	  int rightPeakLoc = 0;
+	  int CalculatedPosition = 0;
+		//int count = 0;
+		//int max_val = 0;
+    debug = 0;
 		initialize(); 
 	//OLED_display_on();
 	//OLED_display_clear();
 	//OLED_display_on();
 
 	while(TRUE){
-		MotorsForward(25);
 		
 		FilterLine();
 
@@ -110,7 +111,8 @@ int main(){
 		
 		//Derivative2();
 		//uart0_put("after Derivative2");
-		
+		//max_val = max();
+
 		if(debug) {
 			if (g_sendData == TRUE) 
 		{
@@ -120,7 +122,7 @@ int main(){
 			uart0_put(str);
 			for (i = 0; i < 128; i++) 
 			{
-				sprintf(str,"%f\n\r", normalized_trace[i]);
+				sprintf(str,"%i\n\r", DiffLine[i]);
 				uart0_put(str);
 			}
 			sprintf(str,"%i\n\r",-2); // end value
@@ -131,44 +133,63 @@ int main(){
 		}
 		//Find left and right edge
 		// gets the peak locations dawg 
-		CalculatePeakLocations();
-		uart0_put("after calculate peaks");
-
-		evaluatePositionANDTurn(leftPeakLoc,rightPeakLoc);
-		uart0_put("after eval and turn");
+		CalculatePeakLocations(&leftPeakLoc,&rightPeakLoc);
+		//uart0_put("after calculate peaks");
+		evaluatePositionANDTurn(&leftPeakLoc,&rightPeakLoc, &CalculatedPosition);
+		if(!debug){
+			sprintf(str,"Left: %i ",leftPeakLoc); // start value
+			uart0_put(str);
+			sprintf(str,"Right: %i ",rightPeakLoc);
+			uart0_put(str);
+			sprintf(str,"Calculated position: %i\r\n",CalculatedPosition);
+			uart0_put(str);			
+		}
+		//uart0_put("after eval and turn");
 		
 		//OLED_display_clear();
 		//OLED_DisplayCameraData(line);
 
-//		sprintf(str,"Left: %i",leftPeakLoc); // start value
-//		uart0_put(str);
-//		sprintf(str,"Right: %i",rightPeakLoc);
-//		uart0_put(str);
-//		sprintf(str,"calculatedPos: %f\n\r", CalculatedPosition );
-//		uart0_put(str);
+		MotorsForward(25);
 		}
 }
 
+
+void FilterLine(){
+	int L = 0;
+	
+	
+	for(L=2;L<127;L++){
+		SmoothLine[L] = (((line[L-2])+ (line[L-1]) + (line[L]) + (line[L+1]) + (line[L+2]))/5);
+		SmoothLine[L+1] = (((line[L-1])+ (line[L]) + (line[L+1]) + (line[L+2]) + (line[L+3]))/5);	
+		DiffLine[L] = abs((SmoothLine[L+1] - SmoothLine[L-1])/2);		
+	}
+		DiffLine[126] = abs((SmoothLine[127] - SmoothLine[125])/2);
+		DiffLine[127] = abs(SmoothLine[127] - SmoothLine[126]);
+}
+
 // find the locations of the peaks 
-void CalculatePeakLocations(){
+void CalculatePeakLocations(int* leftPeakLoc, int* rightPeakLoc){
 	int idx;
 	int TempVal = 999;
 	//tried these starting at 20 but didnt work
-	leftPeakLoc = 64;
-	rightPeakLoc = 64;
+	*leftPeakLoc = 64;
+	*rightPeakLoc = 64;
 	
 	//might want to change this to drop first 20 and last 20 instead of 10
-	for(idx = 10;idx <119; idx++){
+	for(idx = 10;idx <65; idx++){
 		TempVal = DiffLine[idx];
 		//Actual case
-		if(TempVal > DiffLine[leftPeakLoc])
+		if(TempVal > DiffLine[*leftPeakLoc])
 		{
-			leftPeakLoc = idx;
+			*leftPeakLoc = idx;
 		}
+	}
+	for(idx = 65; idx <120; idx++){
+		TempVal = DiffLine[idx];
 		//Actual case
-		if(TempVal<DiffLine[rightPeakLoc])
+		if(TempVal > DiffLine[*rightPeakLoc])
 		{
-			rightPeakLoc=idx;
+			*rightPeakLoc=idx;
 		}
 	}
 //		//Edge case
@@ -181,95 +202,75 @@ void CalculatePeakLocations(){
 //			leftPeakLoc=0;
 //			//rightPeakLoc = 64;
 //		}
-}
-
-
-void FilterLine(){
-	int L = 0;
-	
-	
-	for(L=2;L<127;L++){
-		SmoothLine[L] = (((line[L-2])+ (line[L-1]) + (line[L]) + (line[L+1]) + (line[L+2]))/5);
-		SmoothLine[L+1] = (((line[L-1])+ (line[L]) + (line[L+1]) + (line[L+2]) + (line[L+3]))/5);	
-		DiffLine[L] = ((SmoothLine[L+1] - SmoothLine[L-1])/2);		
-	}
-		DiffLine[126] = (SmoothLine[127] - SmoothLine[125])/2;
-		DiffLine[127] = (SmoothLine[127] - SmoothLine[126]);
 	
 }
 
-void evaluatePositionANDTurn(int LeftLocation, int RightLocation){
-	char streval[200];
+void evaluatePositionANDTurn(int* leftPeakLoc, int* rightPeakLoc, int* CalculatedPosition){
+	//char streval[200];
 	int i =0;
-	int LeftMiss = 1;
-	int RightMiss = 1;
-	//int RightFound = 0;//1 for found 0 for not found
-	//int LeftFound = 0;//1 for found 0 for not found
-	for(i=64; i>0;i--){
-		if(DiffLine[i] > 1500){
+	int rightPeakFound = 0;//1 for found 0 for not found
+	int leftPeakFound = 0;//1 for found 0 for not found
+	for(i=10; i<65;i++){
+		if(DiffLine[i] > 1000){
 			//means we found the left line
 			//LeftFound=1;
-			LeftMiss =0;
+			leftPeakFound = 1;
 		}
 	}
 	
-		for(i=64; i<128;i++){
-		if(DiffLine[i] >1500){
+		for(i=64; i<118;i++){
+		if(DiffLine[i] > 1000){
 			//means we found the right line
 			//RightFound=1;
-			RightMiss =0;
+			rightPeakFound = 1;
 		}
 	}
 		
-	if(LeftLocation == 999 && RightLocation == 999){
-		CalculatedPosition = 64;
+	
+	if(leftPeakFound==1 && rightPeakFound == 1){
+		*CalculatedPosition = *rightPeakLoc-*leftPeakLoc;
+		uart0_put("found both lines/n");
+	
 	}
-	else{
-		if(RightMiss ==0 && LeftMiss == 0){
+	if(leftPeakFound==1 && rightPeakFound == 0){
 			//means that we found both lines
-			dif = RightLocation - LeftLocation;
-			currentLocation = dif/2;
-			CalculatedPosition = LeftLocation + currentLocation;
+			*CalculatedPosition = *leftPeakLoc;
 		}
-		else if(RightMiss ==0 && LeftMiss == 1){
+	 if(leftPeakFound==0 && rightPeakFound == 1){
 			//cant find left but found the right
 			//verify its actually an edge
-			CalculatedPosition = (RightLocation - 64);
+			*CalculatedPosition = *rightPeakLoc;
 		}
-		else if(RightMiss==1 && LeftMiss ==0){
-			CalculatedPosition = (LeftLocation + 64);
-		}
-	
-	}	
-	sprintf(streval,"%f\n\r",CalculatedPosition); // start value
-	uart0_put(streval);
+
+	//sprintf(streval,"%i\n\r",CalculatedPosition); // start value
+	//uart0_put(streval);
 	//on left of the track, need to TURN RIGHT
-	if(CalculatedPosition< 64 )
+	if(*CalculatedPosition< 64 )
 		{
 		//entercode to turn right
 		//TIMER_A2_PWM_DutyCycle(0.05,1);
 		//myDelay(25);
 		//myDelay(25);
 			//adding better logic here for different turn amounts
-			if(CalculatedPosition> 60 ){
-				TurnRightPercent(5);
+			if(*CalculatedPosition> 60 ){
+				TurnRightPercent(100);
 				myDelay(25);
 				//turn slightly bc close to middle (betweem 60 and 64
 				
-			}else if(CalculatedPosition> 55 ){
-				TurnRightPercent(25);
+			}else if(*CalculatedPosition> 50 ){
+				TurnRightPercent(90);
 				myDelay(25);
 				//turn slightly bc close to middle (betweem 55 and 64)
-			}else if(CalculatedPosition> 50 ){
-				TurnRightPercent(55);
+			}else if(*CalculatedPosition> 40 ){
+				TurnRightPercent(75);
 				myDelay(25);
 				//turn slightly harder bc close to middle (betweem 55 and 64)
-			}else if(CalculatedPosition> 45 ){
-				TurnRightPercent(85);
+			}else if(*CalculatedPosition> 30 ){
+				TurnRightPercent(60);
 				myDelay(25);
 				//turn pretty hard bc close to middle (betweem 45 and 64)
 			}else{
-				TurnRightPercent(100);
+				TurnRightPercent(0);
 				myDelay(25);
 				//turn really hard right  bc far to the left of the track
 			}	
@@ -277,30 +278,30 @@ void evaluatePositionANDTurn(int LeftLocation, int RightLocation){
 		
 
 		//on right of the track, need to TURN LEFT
-	else if(CalculatedPosition>64 ){
+	else if(*CalculatedPosition>64 ){
 		//enter code to turn left
 		//TIMER_A2_PWM_DutyCycle(0.1,1);
 		myDelay(25);
 		//myDelay(25);
 					//adding better logic here for different turn amounts
-			if(CalculatedPosition< 65 ){
-				TurnLeftPercent(15);
+			if(*CalculatedPosition < 70 ){
+				TurnLeftPercent(100);
 				myDelay(25);
 				//turn slightly bc close to middle (betweem 65 and 64
-			}else if(CalculatedPosition< 70 ){
-				TurnLeftPercent(25);
+			}else if(*CalculatedPosition< 80 ){
+				TurnLeftPercent(90);
 				myDelay(25);
 				//turn slightly bc close to middle (betweem 70 and 64)
-			}else if(CalculatedPosition< 75 ){
-				TurnLeftPercent(55);
+			}else if(*CalculatedPosition< 90 ){
+				TurnLeftPercent(80);
 				myDelay(25);
 				//turn slightly harder bc close to middle (betweem 75 and 64)
-			}else if(CalculatedPosition< 80 ){
-				TurnLeftPercent(85);
+			}else if(*CalculatedPosition< 100 ){
+				TurnLeftPercent(70);
 				myDelay(25);
 				//turn pretty hard bc close to middle (betweem 80 and 64)
 			}else{
-				TurnLeftPercent(100);
+				TurnLeftPercent(0);
 				myDelay(25);
 				//turn really hard left  bc far to the rightof the track
 			}
@@ -312,7 +313,7 @@ void evaluatePositionANDTurn(int LeftLocation, int RightLocation){
 	}
 	
 	//in center of track, not very likely
-	if(CalculatedPosition==64 ){
+	if(*CalculatedPosition==64 ){
 		TIMER_A2_PWM_DutyCycle(0.075,1);
 		//myDelay(25);
 	}
@@ -346,10 +347,6 @@ void TurnRightPercent(double percentage){
 	
 }
 
-
-
-
-
 double Average(){
     uint32_t t;
     double mean;
@@ -360,6 +357,17 @@ double Average(){
     }
     mean = t/129;
     return(mean);
+}
+
+int max(){
+	int t,i;
+	t = SmoothLine[0];
+	for(i=0; i < 129; i++){
+		if(SmoothLine[i] > t){
+			t = SmoothLine[i];
+		}
+	}
+	return(t);
 }
 
 double CalculateSD(double mean){
